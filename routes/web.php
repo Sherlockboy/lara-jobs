626cdd5e2012e3559541f591907a5b5b9864bd84
Khurshidbek <kabilovhurshid5@gmail.com>
2021-07-31 00:39:11 +0500

    employer dashboard finished

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,8 +23,10 @@ Route::name('listings.')->group(function () {
     Route::post('/new', [ListingController::class, 'store'])->name('store');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard', function (Request $request) {
+    return view('dashboard', [
+        'listings' => $request->user()->listings
+    ]);
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
